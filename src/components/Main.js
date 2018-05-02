c4ee0f2fe7d19fbb64770af4da05867e08a6da65
Ashton <ashchristie@gmail.com>
2018-05-02 15:56:00 -0700

    made header its own component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,60 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { formatMain, streakCalc, centerHeader } from '../helpers/helpers';
 
-export function Date (props) {
+
+class MainHeader extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      welcome: true,
+      showDate: false,
+    }
+  }
+
+  welcomeTimeout = () => {
+    setTimeout(() => {
+      this.setState({ welcome: false })
+      centerHeader();
+    }, 7000);
+  }
+
+  dateTimeout = () => {
+    setTimeout(() => {
+      this.setState({ showDate: true })
+      centerHeader();
+    }, 10000);
+  }
+
+  componentDidMount = () => {
+    centerHeader();
+    this.welcomeTimeout();
+    this.dateTimeout();
+  }
+
+  render () {
+    const { welcome, showDate } = this.state;
+
+    return (window.innerWidth >= 768
+      ? <div className="Main--header">
+          <div className="header-row">
+            <h2 className={`first ${welcome ? 'visible' : 'hidden'}`}>What is the </h2>
+            <h1 className="primary">One Thing</h1>
+            <h2 className={`last ${welcome ? 'visible' : 'hidden'}`}> you can do</h2>
+          </div>
+          <div className="header-row">
+            <h3 className={`first ${welcome ? 'visible' : 'hidden'}`}>to make </h3>
+            <h2 className={`primary ${showDate || welcome ? 'visible' : 'hidden'}`}>{showDate ? <Date /> : 'today'}</h2>
+            <h3 className={`last ${welcome ? 'visible' : 'hidden'}`}> a success?</h3>
+          </div>
+        </div>
+      : <div className="Main--header">
+          <Date />
+        </div>)
+  }
+}
+
+
+function Date (props) {
   const date = formatMain();
 
   return <span className="date">{date}</span>;
@@ -116,53 +169,15 @@ class Main extends React.Component {
     super(props);
     this.state = {
       streak: 0,
-      welcome: true,
-      showDate: false,
     }
   }
 
-  welcomeTimeout = () => {
-    setTimeout(() => {
-      this.setState({ welcome: false })
-      centerHeader();
-    }, 7000);
-  }
-
-  dateTimeout = () => {
-    setTimeout(() => {
-      this.setState({ showDate: true })
-      centerHeader();
-    }, 10000);
-  }
-
-  componentDidMount = () => {
-    centerHeader();
-    this.welcomeTimeout();
-    this.dateTimeout();
-  }
-
   render () {
-    const { welcome, showDate } = this.state;
     const { currentUnix, todayTodo, streak, handleStreak, onSubmit, onDelete, onComplete } = this.props;
 
     return (
       <div className="Main">
-        {window.innerWidth >= 768
-          ? <div className="Main--header">
-            <div className="header-row">
-              <h2 className={`first ${welcome ? 'visible' : 'hidden'}`}>What is the </h2>
-              <h1 className="primary">One Thing</h1>
-              <h2 className={`last ${welcome ? 'visible' : 'hidden'}`}> you can do</h2>
-            </div>
-            <div className="header-row">
-              <h3 className={`first ${welcome ? 'visible' : 'hidden'}`}>to make </h3>
-              <h2 className={`primary ${showDate || welcome ? 'visible' : 'hidden'}`}>{showDate ? <Date /> : 'today'}</h2>
-              <h3 className={`last ${welcome ? 'visible' : 'hidden'}`}> a success?</h3>
-            </div>
-          </div>
-          : <div className="Main--header">
-            <Date />
-          </div>}
+        <MainHeader />
         {todayTodo !== 'complete' &&
           <Form
             currentUnix={currentUnix}
@@ -173,7 +188,7 @@ class Main extends React.Component {
             onComplete={onComplete} />}
         {todayTodo === 'complete' &&
           <div className='complete-msg'>
-            <p>{streak > 1 ? `${streak} days in a row! Great job!` : 'Great job! See you tomorrow.'}</p>
+            <p>Great job! {streak > 1 ? `${streak} days in a row!` : 'Great job! See you tomorrow.'}</p>
           </div>}
           <div>
             <h2 className='quote'>Motivational quote here...</h2>
@@ -181,7 +196,6 @@ class Main extends React.Component {
       </div>
     )
   }
-
 }
 
 Main.propTypes = {
